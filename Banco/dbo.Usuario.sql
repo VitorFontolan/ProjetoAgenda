a2026708eb651d882b14c2440da11d808430f1f7
Vitor Fontolan <72635815+VitorFontolan@users.noreply.github.com>
2023-06-29 15:55:28 -0300

    Add files via upload

---
+++ Banco/dbo.Usuario.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Usuario]
+(
+	[IdUsuario] INT NOT NULL PRIMARY KEY, 
+    [Nome] VARCHAR(50) NOT NULL, 
+    [Login] VARCHAR(20) NOT NULL, 
+    [Senha] VARCHAR(40) NOT NULL, 
+    [DataCadastro] DATETIME NOT NULL
+)
